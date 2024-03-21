77115b63dc91be3c03f4298e2d2e6d8afada994c
Muhammadkhuja Kamoliddinzoda <kamoliddinzoda.mid@phystech.edu>
2024-03-21 21:11:37 +0300

    Class DataBase 1st iterations

---
+++ src/DataBase.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from BaseUser import Base
+from sqlalchemy.orm import sessionmaker
+
+
+class DataBase:
+    def __init__(self, db_url):
+        self.__engine = create_engine(db_url)
+        self.__session = None
+
+    def create_tables(self):
+        Base.metadata.create_all(self.__engine)
+
+    def creat_session(self):
+        session = sessionmaker(bind=self.__engine)
+        self.__session = session()
+
+    def drop_tables(self):
+        Base.metadata.drop_all(self.__engine)
+
+    def query(self, model):
+        return self.__session.query(model)
+
+    def find(self, model, expressions):
+        return len(self.__session.query(model).filter(expressions).all()) != 0
+
+    def insert(self, obj):
+        self.__session.add(obj)
+        self.__session.commit()
+    def delete(self, obj):
+
+
